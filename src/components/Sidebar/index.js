28e439c775c23aa55abd333374ad83b59a8b130b
araceli <carmona.araceli.4690@gmail.com>
2021-11-30 21:54:31 -0800

    icon config

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import './style.css'
-import { Avatar } from '@mui/material'
+import { Avatar, IconButton } from '@mui/material'
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+
 function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="sidebar__header"></div>
-            <Avatar />
-            <div className="sidebar__headerRight">
-                <DonutLargeIcon />
-                <ChatIcon />
-                <MoreVertIcon />
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
+
             <div className="sidebar__search"></div>
             <div className="sidebar__chat"></div>
+
         </div>
     )
 }
