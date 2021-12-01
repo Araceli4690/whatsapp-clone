ed4d9949c11c9ed352457548569ed650a529c339
araceli <carmona.araceli.4690@gmail.com>
2021-11-30 21:40:34 -0800

    material ui install

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './style.css'
+import { Avatar } from '@mui/material'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header"></div>
+            <Avatar />
+            <div className="sidebar__headerRight">
+                <DonutLargeIcon />
+                <ChatIcon />
+                <MoreVertIcon />
+            </div>
             <div className="sidebar__search"></div>
             <div className="sidebar__chat"></div>
         </div>
