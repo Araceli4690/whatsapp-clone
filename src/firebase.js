22841a30b67c3054372953ff5639769530e6ecfb
araceli <carmona.araceli.4690@gmail.com>
2021-11-30 20:38:55 -0800

    react boiler plate files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApOuguhEnAyFpak229_4aKKoLknIZXS2A",
+    authDomain: "whatsapp-clone-dcf49.firebaseapp.com",
+    projectId: "whatsapp-clone-dcf49",
+    storageBucket: "whatsapp-clone-dcf49.appspot.com",
+    messagingSenderId: "670492237671",
+    appId: "1:670492237671:web:f3c7578b2f49a03ad559c2"
+};
