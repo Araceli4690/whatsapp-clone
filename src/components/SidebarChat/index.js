5d3935c9ac89619857170eda96e9eecbf46bcb14
araceli <carmona.araceli.4690@gmail.com>
2021-12-01 20:44:34 -0800

    styling sidebar chat area

--- src/components/SidebarChat/index.js
+++ src/components/SidebarChat/index.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import { Avatar } from '@mui/material'
 
 function SidebarChat() {
+
+    const [seed, setSeed] = useState('');
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000))
+    }, [])
     return (
         <div className="sidebarChat">
-            <Avatar />
-            <div className="sidbarChat__info">
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg `} />
+            <div className="sidebarChat__info">
                 <h2>Room name</h2>
                 <p>Last message...</p>
             </div>
-        </div>
+        </div >
     )
 }
 
