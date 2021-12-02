d34f85e6ae1b6028cd1d6962f712016d8c119372
araceli <carmona.araceli.4690@gmail.com>
2021-12-01 20:56:12 -0800

    add new chat functionality

--- src/components/SidebarChat/index.js
+++ src/components/SidebarChat/index.js
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react'
 import './style.css'
 import { Avatar } from '@mui/material'
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
 
     const [seed, setSeed] = useState('');
+
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, [])
-    return (
+
+    const createChat = () => {
+        const roomName = prompt("Please enter name for chat");
+
+        if (roomName) {
+            // databse stuff
+        }
+    }
+    return !addNewChat ? (
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg `} />
             <div className="sidebarChat__info">
@@ -16,6 +25,10 @@ function SidebarChat() {
                 <p>Last message...</p>
             </div>
         </div >
+    ) : (
+        <div onClick={createChat} className="sidebarChat">
+            <h2>Add new chat</h2>
+        </div>
     )
 }
 
